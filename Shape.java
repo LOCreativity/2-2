d0c6f8736b1886d93345567db277b6098353876b
LOCreativity <105924870+LOCreativity@users.noreply.github.com>
2022-05-30 13:38:04 +0900

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,36 +1,23 @@
 class Shape {
-	private int length;
+	
 	private int firstInt;
 	private int secondInt;
 	private int thirdInt;
-	private int result;
+	
 	
 	public Shape(int firstInt) {
 		this.firstInt = firstInt;
-		this.length = 1;
+		System.out.println("직선의 길이는 : " + this.firstInt);
 	}
 	public Shape(int firstInt, int secondInt) {
 		this.firstInt = firstInt;
 		this.secondInt = secondInt;
-		this.length = 2;
+		System.out.println("사각형의 넓이는 : " + this.firstInt * this.secondInt);
 	}
 	public Shape(int firstInt, int secondInt, int thirdInt) {
 		this.firstInt = firstInt;
 		this.secondInt = secondInt;
 		this.thirdInt = thirdInt;
-		this.length = 3;
-	}
-	public void getArea() {
-
-		if(this.length == 1) {
-			this.result = this.firstInt;
-		}
-		else if(this.length == 2) {
-			this.result = this.firstInt * this.secondInt;
-		}
-		else if(this.length == 3) {
-			this.result = this.firstInt* this.secondInt* this.thirdInt;
-		}
-		return this.result;
+		System.out.println("직육면체의 부피는 : " + this.firstInt * this.secondInt * this.thirdInt);
 	}
 }
